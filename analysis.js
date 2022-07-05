9ceffc934dd08a594036cb64b0e00d417c9a5229
Joe <j.newman@live.co.uk>
2022-07-05 11:59:39 +0100

    All scores and uncounted

--- analysis.js
+++ analysis.js
@@ -3,6 +3,9 @@ class Analysis {
   report(string) {
     let green = 0;
     let amber = 0;
+    let red = 0;
+    let uncounted = 0;
+    let output = []
     let array = string.split(', ')
 
     array.forEach((value) => {
@@ -10,16 +13,19 @@ class Analysis {
         green += 1
       } else if (value === 'Amber') {
         amber += 1
+      } else if (value === 'Red') {
+        red += 1
+      } else {
+        uncounted += 1
       }
     })
 
-    if(green > 0 && amber > 0) {
-      return `Green: ${green}\nAmber: ${amber}`
-    } else if(green === 0) {
-      return `Amber: ${amber}`
-    } else if(amber === 0) {
-      return `Green: ${green}`
-    }
+    if(green > 0) { output.push(`Green: ${green}`) }
+    if(amber > 0) { output.push(`Amber: ${amber}`) }
+    if(red > 0) { output.push(`Red: ${red}`) }
+    if(uncounted > 0) { output.push(`Uncounted: ${uncounted}`) }
+
+    return output.join('\n')
   }
 }
 
--- analysis.test.js
+++ analysis.test.js
@@ -16,13 +16,18 @@ describe(Analysis, () => {
     expect(analysis.report('Green, Green, Green')).toEqual('Green: 3')
   })
 
-  it("returns a string matching 'Green: 3' when 'Green, Green, Green, ' is passed in", () => {
-    let analysis = new Analysis
-    expect(analysis.report('Green, Green, Green, ')).toEqual('Green: 3')
-  })
-
-  it("returns a string matching 'Green: 1, Amber: 1' when 'Green, Amber' is passed in", () => {
+  it("returns a string matching 'Green: 1\nAmber: 1' when 'Green, Amber' is passed in", () => {
     let analysis = new Analysis
     expect(analysis.report('Green, Amber')).toEqual('Green: 1\nAmber: 1')
+  })
+
+  it("returns a string matching 'Green: 2\nAmber: 2' when 'Green, Amber, Green, Amber' is passed in", () => {
+    let analysis = new Analysis
+    expect(analysis.report('Green, Amber, Green, Amber')).toEqual('Green: 2\nAmber: 2')
+  })
+
+  it("returns a string matching 'Green: 1\nAmber: 1\nRed: 1\nUncounted: 1' when 'Green, Amber, Red, Something else' is passed in", () => {
+    let analysis = new Analysis
+    expect(analysis.report('Green, Amber, Red, Something else')).toEqual('Green: 1\nAmber: 1\nRed: 1\nUncounted: 1')
   })
 })
