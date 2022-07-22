ebee6acfe708c515857f680ffbf20e33127f3536
Joe <j.newman@live.co.uk>
2022-07-22 13:09:19 +0100

    2 digit number bug fixed

--- solo/array_addition_1/arrayAddition.js
+++ solo/array_addition_1/arrayAddition.js
@@ -1,8 +1,7 @@
 module.exports = function ArrayAdditionI(arr) { 
   // code goes here  
-  let biggest = arr.sort().pop();
+  let biggest = arr.sort(function(a, b){return a-b}).pop();
   if (arr.length < 2) { return false }
-  return arr;
   for (i = 0; i < arr.length; i++) {
     for (j = i + 1; j < arr.length; j++) {
       if (arr[i] + arr[j] === biggest) { return true}
